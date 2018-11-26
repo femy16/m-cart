1490a3a9701f4d6979e23ae9efc78caf28c6854a
Femy Stephen <femystephen16@gmail.com>
2018-11-26 15:24:38 +0000

    prepareing for heroku

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn mcart.wsgi:application
--- mcart/settings/base.py
+++ mcart/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['m-cart-femystephen.c9users.io']
+ALLOWED_HOSTS = ['m-cart-femystephen.c9users.io','project4-m-cart.herokuapp.com']
 
 
 # Application definition
